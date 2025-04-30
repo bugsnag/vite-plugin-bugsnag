f50c447378bc44ae4bc081a7fe46d09c6ad02e45
Ben Wilson <ben.wilson@smartbear.com>
2025-04-30 14:10:21 +0100

    configure unit testing

--- src/build-reporter-plugin.ts
+++ src/build-reporter-plugin.ts
@@ -1,7 +1,81 @@
 import type { Plugin } from 'vite'
+import Bugsnag, { BugsnagCreateBuildOptions } from '@bugsnag/cli'
 
-export function BugsnagBuildReporterPlugin(): Plugin {
+const LOG_PREFIX = '[BugsnagBuildReporterPlugin]'
+
+export function BugsnagBuildReporterPlugin (configOptions: BugsnagBuildReporterPluginOptions): Plugin {
+  const buildOptions = getBuildOptions(configOptions)
+
   return {
-    name: 'vite-plugin-bugsnag-build-reporter'
+    name: 'vite-plugin-bugsnag-build-reporter',
+    buildEnd (error) {
+      const logger = configOptions.logger || this.environment.logger
+
+      if (error) {
+        logger.error(`${LOG_PREFIX}build failed with error: ${error}`)
+        return
+      }
+
+      logger.info(`${LOG_PREFIX}creating build for version "${buildOptions.versionName}" using the bugsnag-cli`)
+
+      Bugsnag.CreateBuild(buildOptions, process.cwd())
+        .then((output) => {
+            output.split('\n').forEach((line) => {
+              logger.info(LOG_PREFIX + line)
+            })
+        })
+        .catch((err) => {
+          err.toString().split('\n').forEach((line: string) => {
+            logger.error(LOG_PREFIX + line)
+          })
+        })
+    }
   }
+}
+
+function getBuildOptions (configOptions: BugsnagBuildReporterPluginOptions) {
+  const buildOptions: BugsnagCreateBuildOptions = {
+    apiKey: configOptions.apiKey,
+    versionName: configOptions.appVersion,
+    autoAssignRelease: configOptions.autoAssignRelease,
+    builderName: configOptions.builderName,
+    metadata: configOptions.metadata,
+    releaseStage: configOptions.releaseStage,
+    provider: configOptions.sourceControl?.provider,
+    repository: configOptions.sourceControl?.repository,
+    revision: configOptions.sourceControl?.revision,
+    buildApiRootUrl: configOptions.endpoint,
+    logLevel: configOptions.logLevel
+  }
+
+  for (const [key, value] of Object.entries(buildOptions)) {
+    if (value === undefined) {
+      delete buildOptions[key as keyof BugsnagCreateBuildOptions]
+    }
+  }
+
+  return buildOptions
+}
+
+export interface BugsnagBuildReporterPluginOptions {
+  apiKey: string
+  appVersion: string
+  autoAssignRelease?: boolean
+  builderName?: string
+  metadata?: object
+  releaseStage?: string
+  sourceControl?: {
+    provider?: string
+    repository?: string
+    revision?: string
+  }
+  logLevel?: string
+  logger?: {
+    debug: (message: string) => void
+    info: (message: string) => void
+    warn: (message: string) => void
+    error: (message: string) => void
+  }
+  path?: string
+  endpoint?: string
 }
--- test/build-reporter-plugin.test.ts
+++ test/build-reporter-plugin.test.ts
@@ -1,8 +1,53 @@
-import { BugsnagBuildReporterPlugin } from '../'
+import fs from 'fs'
+import { resolve } from 'path'
+import { build } from 'vite'
+import { describe, expect, test, vi } from 'vitest'
+import { BugsnagBuildReporterPlugin } from '../src/build-reporter-plugin'
+
+vi.mock('@bugsnag/cli', () => ({
+    default: {
+        CreateBuild: vi.fn(() => Promise.resolve('Build reported successfully'))
+    }
+}))
 
 describe('BugsnagBuildReporterPlugin', () => {
-    it('should return a valid plugin object', () => {
-        const plugin = BugsnagBuildReporterPlugin()
-        expect(plugin.name).toBe('vite-plugin-bugsnag-build-reporter')
+    test('should report the build successfully', async () => {
+        const mockLogger = {
+            info: vi.fn(),
+            error: vi.fn(),
+            warn: vi.fn(),
+            debug: vi.fn()
+        }
+
+        const plugin = BugsnagBuildReporterPlugin({
+            apiKey: 'test-api-key',
+            appVersion: '1.2.3',
+            builderName: 'test-builder',
+            releaseStage: 'production',
+            sourceControl: {
+                provider: 'github',
+                repository: 'test-repo',
+                revision: 'test-revision'
+            },
+            logger: mockLogger
+        })
+
+        const fixturesPath = resolve(__dirname, '../test/fixtures/vite/build-reporter/successful-upload')
+        const viteConfig = {
+            root: fixturesPath,
+            plugins: [plugin]
+        }
+
+        // Ensure the output directory is clean
+        const outputDir = resolve(fixturesPath, 'dist')
+        if (fs.existsSync(outputDir)) {
+            fs.rmSync(outputDir, { recursive: true, force: true })
+        }
+
+        await build(viteConfig)
+
+        expect(mockLogger.info).toHaveBeenCalledWith(
+            '[BugsnagBuildReporterPlugin]creating build for version "1.2.3" using the bugsnag-cli'
+        )
     })
 })
